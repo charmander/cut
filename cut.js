3444b7cc597c2e9afc349e74c8f466061d398fe9
Charmander <campersander@gmail.com>
2014-04-25 18:22:59 -0700

    Add and apply JSHint settings.

--- cut.js
+++ cut.js
@@ -1,11 +1,13 @@
-"use strict";
+'use strict';
 
-var Cut = (function() {
+/* exported Cut */
+
+var Cut = (function () {
 	function EventEmitter() {
 		this.events = {};
 	}
 
-	EventEmitter.prototype.on = function(eventName, listener) {
+	EventEmitter.prototype.on = function (eventName, listener) {
 		if (this.events.hasOwnProperty(eventName)) {
 			this.events[eventName].push(listener);
 		} else {
@@ -13,7 +15,7 @@ var Cut = (function() {
 		}
 	};
 
-	EventEmitter.prototype.emit = function(eventName, data) {
+	EventEmitter.prototype.emit = function (eventName, data) {
 		if (this.events.hasOwnProperty(eventName)) {
 			var firing = this.events[eventName].slice();
 
@@ -25,7 +27,7 @@ var Cut = (function() {
 		}
 	};
 
-	EventEmitter.prototype.off = function(eventName, listener) {
+	EventEmitter.prototype.off = function (eventName, listener) {
 		if (this.events.hasOwnProperty(eventName)) {
 			var listeners = this.events[eventName];
 			var i = listeners.indexOf(listener);
@@ -41,8 +43,8 @@ var Cut = (function() {
 
 		var cut = this;
 
-		this.imageWidth = +imageInput.getAttribute("width");
-		this.imageHeight = +imageInput.getAttribute("height");
+		this.imageWidth = +imageInput.getAttribute('width');
+		this.imageHeight = +imageInput.getAttribute('height');
 		this.cropWidth = options.cropWidth;
 		this.cropHeight = options.cropHeight;
 
@@ -50,18 +52,18 @@ var Cut = (function() {
 		this.scaleContain = Math.min(1, isImageThin ? options.cropHeight / this.imageHeight : options.cropWidth / this.imageWidth);
 		this.scaleCover = Math.min(1, !isImageThin ? options.cropHeight / this.imageHeight : options.cropWidth / this.imageWidth);
 
-		var imageElement = document.createElement("img");
-		imageElement.className = "cut-image";
+		var imageElement = document.createElement('img');
+		imageElement.className = 'cut-image';
 		imageElement.src = imageInput.src;
 		imageElement.width = this.imageWidth;
 		imageElement.height = this.imageHeight;
 
-		var cropBox = document.createElement("div");
-		cropBox.className = "cut-crop-box";
-		cropBox.style.width = this.cropWidth + "px";
-		cropBox.style.height = this.cropHeight + "px";
-		cropBox.style.marginLeft = (-this.cropWidth / 2) + "px";
-		cropBox.style.marginTop = (-this.cropHeight / 2) + "px";
+		var cropBox = document.createElement('div');
+		cropBox.className = 'cut-crop-box';
+		cropBox.style.width = this.cropWidth + 'px';
+		cropBox.style.height = this.cropHeight + 'px';
+		cropBox.style.marginLeft = (-this.cropWidth / 2) + 'px';
+		cropBox.style.marginTop = (-this.cropHeight / 2) + 'px';
 		cropBox.style.backgroundImage = 'url("' + encodeURI(imageElement.src) + '")';
 
 		this.imageElement = imageElement;
@@ -69,16 +71,16 @@ var Cut = (function() {
 		this.scaleMinimum = options.scaleMinimum == null ? this.scaleContain : +options.scaleMinimum;
 		this.scaleMaximum = +options.scaleMaximum || 1;
 
-		var container = document.createElement("div");
-		container.className = "cut-container";
+		var container = document.createElement('div');
+		container.className = 'cut-container';
 		container.tabIndex = 0;
-		container.style.minHeight = this.cropHeight + "px";
+		container.style.minHeight = this.cropHeight + 'px';
 
 		this.zoom = 1;
 		this.offsetX = 0;
 		this.offsetY = 0;
 
-		container.addEventListener("mousedown", function(e) {
+		container.addEventListener('mousedown', function (e) {
 			if (e.button || e.ctrlKey || e.shiftKey || e.metaKey || e.altKey) {
 				return;
 			}
@@ -89,8 +91,8 @@ var Cut = (function() {
 			var originalOffsetY = cut.offsetY;
 
 			function stopDrag(e) {
-				window.removeEventListener("mousemove", drag, false);
-				window.removeEventListener("mouseup", stopDrag, false);
+				window.removeEventListener('mousemove', drag, false);
+				window.removeEventListener('mouseup', stopDrag, false);
 
 				drag(e);
 			}
@@ -102,19 +104,19 @@ var Cut = (function() {
 				cut.offsetX = originalOffsetX + deltaX;
 				cut.offsetY = originalOffsetY + deltaY;
 				cut.positionImage();
-				cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+				cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 
 				e.preventDefault();
 			}
 
-			window.addEventListener("mousemove", drag, false);
-			window.addEventListener("mouseup", stopDrag, false);
+			window.addEventListener('mousemove', drag, false);
+			window.addEventListener('mouseup', stopDrag, false);
 
 			e.preventDefault();
 			container.focus();
 		}, false);
 
-		container.addEventListener("touchstart", function(e) {
+		container.addEventListener('touchstart', function (e) {
 			if (e.touches.length > 2) {
 				return;
 			}
@@ -138,8 +140,8 @@ var Cut = (function() {
 			}
 
 			function stopDrag() {
-				window.removeEventListener("touchmove", drag, false);
-				window.removeEventListener("touchend", stopDrag, false);
+				window.removeEventListener('touchmove', drag, false);
+				window.removeEventListener('touchend', stopDrag, false);
 			}
 
 			function drag(e) {
@@ -168,19 +170,19 @@ var Cut = (function() {
 					cut.positionImage();
 				}
 
-				cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+				cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 
 				e.preventDefault();
 			}
 
-			window.addEventListener("touchmove", drag, false);
-			window.addEventListener("touchend", stopDrag, false);
+			window.addEventListener('touchmove', drag, false);
+			window.addEventListener('touchend', stopDrag, false);
 
 			e.preventDefault();
 			container.focus();
 		}, false);
 
-		container.addEventListener("keydown", function(e) {
+		container.addEventListener('keydown', function (e) {
 			if (e.shiftKey || e.ctrlKey || e.metaKey || e.altKey) {
 				return;
 			}
@@ -189,25 +191,25 @@ var Cut = (function() {
 				case 37: // Left
 					cut.offsetX -= 5;
 					cut.positionImage();
-					cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+					cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 					break;
 
 				case 38: // Up
 					cut.offsetY -= 5;
 					cut.positionImage();
-					cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+					cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 					break;
 
 				case 39: // Right
 					cut.offsetX += 5;
 					cut.positionImage();
-					cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+					cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 					break;
 
 				case 40: // Down
 					cut.offsetY += 5;
 					cut.positionImage();
-					cut.emit("move", { x: cut.offsetX, y: cut.offsetY });
+					cut.emit('move', { x: cut.offsetX, y: cut.offsetY });
 					break;
 
 				default:
@@ -217,10 +219,10 @@ var Cut = (function() {
 			e.preventDefault();
 		}, false);
 
-		container.addEventListener("keypress", function(e) {
-			if (e.key === "-") {
+		container.addEventListener('keypress', function (e) {
+			if (e.key === '-') {
 				cut.zoomTo(cut.zoom - 0.05);
-			} else if (e.key === "+") {
+			} else if (e.key === '+') {
 				cut.zoomTo(cut.zoom + 0.05);
 			} else {
 				return;
@@ -240,12 +242,12 @@ var Cut = (function() {
 	Cut.prototype = Object.create(EventEmitter.prototype);
 	Cut.prototype.constructor = Cut;
 
-	Cut.prototype.positionImage = function() {
+	Cut.prototype.positionImage = function () {
 		var imageTransform =
-			"translate(" +
-				(-this.imageWidth / 2 + this.offsetX).toFixed(1) + "px, " +
-				(-this.imageHeight / 2 + this.offsetY).toFixed(1) + "px) " +
-			"scale(" + this.zoom.toFixed(2) + ")";
+			'translate(' +
+				(-this.imageWidth / 2 + this.offsetX).toFixed(1) + 'px, ' +
+				(-this.imageHeight / 2 + this.offsetY).toFixed(1) + 'px) ' +
+			'scale(' + this.zoom.toFixed(2) + ')';
 
 		this.imageElement.style.webkitTransform = imageTransform;
 		this.imageElement.style.msTransform = imageTransform;
@@ -253,11 +255,11 @@ var Cut = (function() {
 		this.imageElement.style.transform = imageTransform;
 
 		this.cropBox.style.backgroundPosition =
-			(-this.imageWidth / 2 * this.zoom + this.cropWidth / 2 + this.offsetX).toFixed(1) + "px " +
-			(-this.imageHeight / 2 * this.zoom + this.cropHeight / 2 + this.offsetY).toFixed(1) + "px";
+			(-this.imageWidth / 2 * this.zoom + this.cropWidth / 2 + this.offsetX).toFixed(1) + 'px ' +
+			(-this.imageHeight / 2 * this.zoom + this.cropHeight / 2 + this.offsetY).toFixed(1) + 'px';
 	};
 
-	Cut.prototype.zoomTo = function(zoom) {
+	Cut.prototype.zoomTo = function (zoom) {
 		if (zoom < this.scaleMinimum) {
 			zoom = this.scaleMinimum;
 		} else if (zoom > this.scaleMaximum) {
@@ -267,18 +269,18 @@ var Cut = (function() {
 		this.offsetX *= zoom / this.zoom;
 		this.offsetY *= zoom / this.zoom;
 		this.zoom = zoom;
-		this.cropBox.style.backgroundSize = (this.imageWidth * zoom).toFixed(1) + "px auto";
+		this.cropBox.style.backgroundSize = (this.imageWidth * zoom).toFixed(1) + 'px auto';
 		this.positionImage();
-		this.emit("zoom", zoom);
+		this.emit('zoom', zoom);
 	};
 
-	Cut.prototype.cover = function() {
+	Cut.prototype.cover = function () {
 		this.offsetX = 0;
 		this.offsetY = 0;
 		this.zoomTo(this.scaleCover);
 	};
 
-	Cut.prototype.contain = function() {
+	Cut.prototype.contain = function () {
 		this.offsetX = 0;
 		this.offsetY = 0;
 		this.zoomTo(this.scaleContain);
