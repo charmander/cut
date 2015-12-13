41ef7757318a5b1c27c976c7c3f2be816d9c45ab
Charmander <~@charmander.me>
2015-12-13 05:00:00 +0000

    Centre demo slider and remove buttons
    Covers by default; contain is the smallest possible scale.
    Also removes outline hack for Firefox.

--- demo/static/demo.js
+++ demo/static/demo.js
@@ -1,88 +1,65 @@
-'use strict';
-
 /* globals Cut */
 
-var form = document.getElementById('cut');
-var cutImage = document.getElementById('cut-image');
+(function () {
+	'use strict';
 
-var cut = new Cut(cutImage, {
-	cropWidth: 120,
-	cropHeight: 120
-});
+	var form = document.getElementById('cut');
+	var cutImage = document.getElementById('cut-image');
 
-var controls = document.createElement('div');
-controls.id = 'controls';
+	var cut = new Cut(cutImage, {
+		cropWidth: 120,
+		cropHeight: 120
+	});
 
-var slider = document.createElement('input');
-slider.id = 'zoom-slider';
-slider.type = 'range';
-slider.min = cut.scaleMinimum.toFixed(2);
-slider.max = 1;
-slider.step = 0.01;
-slider.value = 1;
+	var controls = document.createElement('div');
+	controls.id = 'controls';
 
-function sliderChanged() {
-	var zoom = +slider.value;
+	var slider = document.createElement('input');
+	slider.id = 'zoom-slider';
+	slider.type = 'range';
+	slider.min = cut.scaleMinimum.toFixed(2);
+	slider.max = 1;
+	slider.step = 0.01;
+	slider.value = 1;
 
-	if (zoom < +slider.min) {
-		zoom = +slider.min;
-	} else if (!(zoom <= 1)) {
-		zoom = 1;
-	}
+	function sliderChanged() {
+		var zoom = +slider.value;
 
-	cut.zoomTo(zoom);
-}
+		if (zoom < +slider.min) {
+			zoom = +slider.min;
+		} else if (zoom > 1 || isNaN(zoom)) {
+			zoom = 1;
+		}
 
-slider.addEventListener('input', sliderChanged, false);
-slider.addEventListener('change', sliderChanged, false);
-
-cut.on('zoom', function (zoom) {
-	slider.value = zoom;
-});
-
-var buttons = document.createElement('span');
-
-var containButton = document.createElement('input');
-containButton.id = 'contain-button';
-containButton.className = 'button';
-containButton.type = 'button';
-containButton.title = 'Contain';
-containButton.value = '∈';
-
-containButton.addEventListener('click', function () {
-	cut.contain();
-}, false);
-
-var coverButton = document.createElement('input');
-coverButton.id = 'cover-button';
-coverButton.className = 'button';
-coverButton.type = 'button';
-coverButton.title = 'Cover';
-coverButton.value = '∋';
-
-coverButton.addEventListener('click', function () {
-	cut.cover();
-}, false);
-
-buttons.appendChild(containButton);
-buttons.appendChild(coverButton);
-controls.appendChild(slider);
-controls.appendChild(buttons);
-form.appendChild(controls);
-
-var cropCoordinates = document.getElementById('crop-coordinates');
-
-if (cropCoordinates) {
-	cropCoordinates.parentNode.removeChild(cropCoordinates);
-
-	if (cropCoordinates.classList.contains('ended')) {
-		var startX = parseInt(cropCoordinates.style.left, 10);
-		var startY = parseInt(cropCoordinates.style.top, 10);
-		var width = parseInt(cropCoordinates.style.width, 10);
-		var zoom = 120 / width;
-
-		cut.offsetX = (cutImage.width / 2 - startX) * zoom - 60;
-		cut.offsetY = (cutImage.height / 2 - startY) * zoom - 60;
 		cut.zoomTo(zoom);
 	}
-}
+
+	slider.addEventListener('input', sliderChanged, false);
+	slider.addEventListener('change', sliderChanged, false);
+
+	cut.on('zoom', function (zoom) {
+		slider.value = zoom;
+	});
+
+	cut.cover();
+
+	controls.appendChild(slider);
+	form.appendChild(controls);
+
+	var cropCoordinates = document.getElementById('crop-coordinates');
+
+	if (cropCoordinates) {
+		cropCoordinates.parentNode.removeChild(cropCoordinates);
+
+		if (cropCoordinates.classList.contains('ended')) {
+			var startX = parseInt(cropCoordinates.style.left, 10);
+			var startY = parseInt(cropCoordinates.style.top, 10);
+			var width = parseInt(cropCoordinates.style.width, 10);
+			var zoom = 120 / width;
+
+			cut.offsetX = (cutImage.width / 2 - startX) * zoom - 60;
+			cut.offsetY = (cutImage.height / 2 - startY) * zoom - 60;
+			cut.zoomTo(zoom);
+		}
+	}
+})();
